a1dcfcb5999a49dc9abd4169085ff2530b38c4ba
kNelakonda <knelakon@ucsd.edu>
2023-05-20 22:20:27 -0700

    a fix is being attempted

--- sw.js
+++ sw.js
@@ -48,7 +48,7 @@ self.addEventListener("fetch", function (event) {
       return cache.match(event.request).then((cachedResponse) => {
         return (
           cachedResponse ||
-          fetch(event.request.url).then((fetchedResponse) => {
+          fetch(event.request).then((fetchedResponse) => {
             cache.put(event.request, fetchedResponse.clone());
 
             return fetchedResponse;
